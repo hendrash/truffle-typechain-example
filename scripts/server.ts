82f7a57f638475af0a336bf3e601dc5b2d18bb72
hendrash <hendrash@mail.gvsu.edu>
2021-06-05 19:12:05 -0400

    NO MORE

--- scripts/server.ts
+++ scripts/server.ts
@@ -5,8 +5,10 @@ import { network, TestNet } from "./globals"
 
 async function server() {
     const bridgeEvent = new web3Provider.eth.Contract((BridgeEvent as any), network().BridgeEventContract)
-    let i = await bridgeEvent.methods
-    console.log(await bridgeEvent.methods)
+    // console.log(bridgeEvent)
+    console.log(await bridgeEvent.methods.bridgeUSDC('1').send({
+        from: '0x276e4B2EdE2a6d6A30A9E4453f9Ef6d69FFf3CD8'
+    }))
 }
 
 server();
