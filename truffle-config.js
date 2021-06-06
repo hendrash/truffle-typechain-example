6879a3009e95a9b5c450d92ec0462b021d8ee6bb
hendrash <hendrash@mail.gvsu.edu>
2021-06-05 22:50:06 -0400

    fix config file

--- truffle-config.js
+++ truffle-config.js
@@ -1,52 +1,5 @@
 require("ts-node/register");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-let fs = require("fs");
-const mnemonic = fs
-  .readFileSync(".secret")
-  .toString()
-  .trim();
-let versionManager = (branch) => {
-  if (branch === "main_net")
-    return {
-      host: "localhost",
-      port: 8545,
-      network_id: "*",
-      gas: 5000000,
-      branch_name: "main_net",
-    };
-  if (branch === "test_net")
-    return {
-      host: "https://data-seed-prebsc-1-s1.binance.org",
-      port: 8545,
-      network_id: "*",
-      gas: 5000000,
-      branch_name: "test_net",
-    };
-  if (branch === "local_net")
-    return {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-      gas: 5000000,
-      branch_name: "local_net",
-    };
-  else console.error("No specified branch configuration may not work");
-};
-const writeToFile = (dev) => {
-  fs.writeFile("scripts/configuration.json", JSON.stringify(dev) + "", function(
-    err
-  ) {
-    if (err) {
-      console.error(err);
-    }
-  });
-  return dev;
-};
-
-dev = () => {
-  const branchName = require("current-git-branch");
-  return writeToFile(versionManager(branchName()));
-};
 module.exports = {
   networks: {
     development: {
@@ -56,7 +9,6 @@ module.exports = {
       gas: 5000000,
       branch_name: "local_net",
     },
-    dev: dev(),
     testnet: {
       provider: () =>
         new HDWalletProvider(
